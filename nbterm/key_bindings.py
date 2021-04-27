79bd1b562a8ab54eadd4fa7de9f8edf5b0b5432c
Nick Tallant <ndtallant@gmail.com>
2021-04-27 18:34:53 -0500

    Can't necessarily unpack multi-key bindings

--- nbterm/key_bindings.py
+++ nbterm/key_bindings.py
@@ -42,15 +42,15 @@ class KeyBindings:
         def command_mode() -> bool:
             return not self.edit_mode
 
-        @self.key_bindings.add(*BINDINGS["exit"])
+        @self.key_bindings.add(BINDINGS["exit"])
         async def kb_exit(event):
             await self.exit()
 
-        @self.key_bindings.add(*BINDINGS["save"], filter=command_mode)
+        @self.key_bindings.add(BINDINGS["save"], filter=command_mode)
         def kb_save(event):
             self.save()
 
-        @self.key_bindings.add(*BINDINGS["enter_cell_to_edit"], filter=command_mode)
+        @self.key_bindings.add(BINDINGS["enter_cell_to_edit"], filter=command_mode)
         def kb_enter(event):
             self.enter_cell()
 
@@ -58,62 +58,62 @@ class KeyBindings:
         def kb_escape(event):
             self.exit_cell()
 
-        @self.key_bindings.add(*BINDINGS["go_up"], filter=command_mode)
+        @self.key_bindings.add(BINDINGS["go_up"], filter=command_mode)
         def kb_go_up(event):
             self.go_up()
 
-        @self.key_bindings.add(*BINDINGS["go_down"], filter=command_mode)
+        @self.key_bindings.add(BINDINGS["go_down"], filter=command_mode)
         def kb_down(event):
             self.go_down()
 
-        @self.key_bindings.add(*BINDINGS["move_cell_up"], filter=command_mode)
+        @self.key_bindings.add(BINDINGS["move_cell_up"], filter=command_mode)
         def kb_move_up(event):
             self.move_up()
 
-        @self.key_bindings.add(*BINDINGS["move_cell_down"], filter=command_mode)
+        @self.key_bindings.add(BINDINGS["move_cell_down"], filter=command_mode)
         def kb_move_down(event):
             self.move_down()
 
-        @self.key_bindings.add(*BINDINGS["clear_output"], filter=command_mode)
+        @self.key_bindings.add(BINDINGS["clear_output"], filter=command_mode)
         def kb_clear_output(event):  # noqa
             self.clear_output()
 
-        @self.key_bindings.add(*BINDINGS["make_cell_markdown"], filter=command_mode)
+        @self.key_bindings.add(BINDINGS["make_cell_markdown"], filter=command_mode)
         def kb_markdown_cell(event):
             self.markdown_cell()
 
-        @self.key_bindings.add(*BINDINGS["make_cell_code"], filter=command_mode)
+        @self.key_bindings.add(BINDINGS["make_cell_code"], filter=command_mode)
         def kb_code_cell(event):
             self.code_cell()
 
-        @self.key_bindings.add(*BINDINGS["run_cell_and_stay"], filter=command_mode)
+        @self.key_bindings.add(BINDINGS["run_cell_and_stay"], filter=command_mode)
         async def kb_queue_run_cell(event):
             await self.queue_run_cell()
 
-        @self.key_bindings.add(*BINDINGS["run_cell_and_select_below"], filter=command_mode)
+        @self.key_bindings.add(BINDINGS["run_cell_and_select_below"], filter=command_mode)
         async def kb_queue_run_cell_and_select_below(event):
             await self.queue_run_cell(and_select_below=True)
 
-        @self.key_bindings.add(*BINDINGS["cut_cell"], filter=command_mode)
+        @self.key_bindings.add(BINDINGS["cut_cell"], filter=command_mode)
         def kb_cut_cell(event):
             self.cut_cell()
 
-        @self.key_bindings.add(*BINDINGS["copy_cell"], filter=command_mode)
+        @self.key_bindings.add(BINDINGS["copy_cell"], filter=command_mode)
         def kb_copy_cell(event):
             self.copy_cell()
 
-        @self.key_bindings.add(*BINDINGS["paste_cell_above"], filter=command_mode)
+        @self.key_bindings.add(BINDINGS["paste_cell_above"], filter=command_mode)
         def kb_paste_cell(event):
             self.paste_cell()
 
-        @self.key_bindings.add(*BINDINGS["paste_cell_below"], filter=command_mode)
+        @self.key_bindings.add(BINDINGS["paste_cell_below"], filter=command_mode)
         def kb_paste_cell_under(event):
             self.paste_cell(below=True)
 
-        @self.key_bindings.add(*BINDINGS["insert_cell_above"], filter=command_mode)
+        @self.key_bindings.add(BINDINGS["insert_cell_above"], filter=command_mode)
         def kb_insert_cell(event):
             self.insert_cell()
 
-        @self.key_bindings.add(*BINDINGS["insert_cell_below"], filter=command_mode)
+        @self.key_bindings.add(BINDINGS["insert_cell_below"], filter=command_mode)
         def kb_insert_cell_bellow(event):
             self.insert_cell(below=True)
