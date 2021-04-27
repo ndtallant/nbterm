0d2c314f81a8fd91a5b40df24f711fa3dc9ace65
Nick Tallant <ndtallant@gmail.com>
2021-04-27 17:43:31 -0500

    Change key binding function names to be generic

--- nbterm/key_bindings.py
+++ nbterm/key_bindings.py
@@ -15,77 +15,77 @@ class KeyBindings:
             return not self.edit_mode
 
         @self.key_bindings.add("c-q")
-        async def c_q(event):
+        async def kb_exit(event):
             await self.exit()
 
         @self.key_bindings.add("c-s", filter=command_mode)
-        def c_s(event):
+        def kb_save(event):
             self.save()
 
         @self.key_bindings.add("enter", filter=command_mode)
-        def enter(event):
+        def kb_enter(event):
             self.enter_cell()
 
         @self.key_bindings.add("escape", filter=edit_mode, eager=True)
-        def escape(event):
+        def kb_escape(event):
             self.exit_cell()
 
         @self.key_bindings.add("up", filter=command_mode)
-        def up(event):
+        def kb_go_up(event):
             self.go_up()
 
         @self.key_bindings.add("down", filter=command_mode)
-        def down(event):
+        def kb_down(event):
             self.go_down()
 
         @self.key_bindings.add("c-up", filter=command_mode)
-        def c_up(event):
+        def kb_move_up(event):
             self.move_up()
 
         @self.key_bindings.add("c-down", filter=command_mode)
-        def c_down(event):
+        def kb_move_down(event):
             self.move_down()
 
         @self.key_bindings.add("l", filter=command_mode)
-        def l(event):  # noqa
+        def kb_clear_output(event):  # noqa
             self.clear_output()
 
         @self.key_bindings.add("m", filter=command_mode)
-        def m(event):
+        def kb_markdown_cell(event):
             self.markdown_cell()
 
         @self.key_bindings.add("o", filter=command_mode)
-        def o(event):
+        def kb_code_cell(event):
             self.code_cell()
 
         @self.key_bindings.add("c-e", filter=command_mode)
-        async def c_e(event):
+        async def kb_queue_run_cell(event):
             await self.queue_run_cell()
 
         @self.key_bindings.add("c-r", filter=command_mode)
-        async def c_r(event):
+        async def kb_queue_run_cell_and_select_below(event):
             await self.queue_run_cell(and_select_below=True)
 
         @self.key_bindings.add("x", filter=command_mode)
-        def x(event):
+        def kb_cut_cell(event):
             self.cut_cell()
 
         @self.key_bindings.add("c", filter=command_mode)
-        def c(event):
+        def kb_copy_cell(event):
             self.copy_cell()
 
         @self.key_bindings.add("c-v", filter=command_mode)
-        def c_v(event):
+        def kb_paste_cell(event):
             self.paste_cell()
 
         @self.key_bindings.add("v", filter=command_mode)
-        def v(event):
+        def kb_paste_cell_under(event):
             self.paste_cell(below=True)
 
         @self.key_bindings.add("a", filter=command_mode)
-        def a(event):
+        def kb_insert_cell(event):
             self.insert_cell()
 
         @self.key_bindings.add("b", filter=command_mode)
-        def b(event):
+        def kb_insert_cell_bellow(event):
             self.insert_cell(below=True)
